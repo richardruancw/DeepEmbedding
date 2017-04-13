7d28d87a60854f1f3b13a04bd287896b34d2fbc3
Yue Liu <benliu20131124@gmail.com>
2017-04-12 22:15:04 -0700

    modified collapse code to add weight

--- collapse.py
+++ collapse.py
@@ -5,8 +5,8 @@ sc = SparkContext()
 table1 = sc.textFile("little_pins.tsv").map(lambda line: line.split("\t")).map(lambda list: (list[2], list[1]))
 table2 = sc.textFile("little_pins.tsv").map(lambda line: line.split("\t")).map(lambda list: (list[2], list[1]))
 
-graph = table1.join(table2).map(lambda (board, (pin1, pin2)): sorted((pin1, pin2))).filter(lambda pins : pins[0] != pins[1])\
-.map(lambda pins: " ".join(pins)).distinct()
+graph = table1.join(table2).map(lambda (board, (pin1, pin2)): tuple(sorted((pin1, pin2)))).filter(lambda pins : pins[0] != pins[1])\
+.map(lambda pins: (pins, 1)).distinct().reduceByKey(lambda v1,v2: v1+v2).map(lambda (pair, count): " ".join([pair[0], pair[1], str(count)]))
 
 graph.saveAsTextFile("graph")
 
