1f878c91767354b8f4776ee60601cf2ff4dcceb3
Rciahrdruancw <ruanchuanwei@gmail.com>
2017-04-13 17:30:31 -0700

    delete sample graph

--- snap/examples/deepembed/Makefile.ex
+++ snap/examples/deepembed/Makefile.ex
@@ -3,9 +3,9 @@
 
 ## Main application file
 MAIN = deepembed
-DEPH = $(EXSNAPADV)/n2v.h $(EXSNAPADV)/word2vec.h $(EXSNAPADV)/biasedrandomwalk.h $ deeputils.h samplegraph.h recoveredges.h linearinterpolation.h 
+DEPH = $(EXSNAPADV)/n2v.h $(EXSNAPADV)/word2vec.h $(EXSNAPADV)/biasedrandomwalk.h $ deeputils.h recoveredges.h linearinterpolation.h 
 
-DEPCPP = $(EXSNAPADV)/n2v.cpp $(EXSNAPADV)/word2vec.cpp $(EXSNAPADV)/biasedrandomwalk.cpp $ deeputils.cpp samplegraph.cpp recoveredges.cpp linearinterpolation.cpp
+DEPCPP = $(EXSNAPADV)/n2v.cpp $(EXSNAPADV)/word2vec.cpp $(EXSNAPADV)/biasedrandomwalk.cpp $ deeputils.cpp recoveredges.cpp linearinterpolation.cpp
 
 CXXFLAGS += $(CXXOPENMP)
  
