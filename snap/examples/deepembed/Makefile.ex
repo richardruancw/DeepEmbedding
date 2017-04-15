905421f24ad79cbccac0b851ae330c032ee2ed8b
Rciahrdruancw <ruanchuanwei@gmail.com>
2017-04-14 19:59:18 -0700

    fix link error

--- snap/examples/deepembed/Makefile.ex
+++ snap/examples/deepembed/Makefile.ex
@@ -3,9 +3,11 @@
 
 ## Main application file
 MAIN = deepembed
-DEPH = $(EXSNAPADV)/n2v.h $(EXSNAPADV)/word2vec.h $(EXSNAPADV)/biasedrandomwalk.h $ deeputils.h recoveredges.h linearinterpolation.h samplen2v.h jumpword2vec.h
+DEPH = $(EXSNAPADV)/n2v.h $(EXSNAPADV)/word2vec.h $(EXSNAPADV)/biasedrandomwalk.h $ deeputils.h recoveredges.h linearinterpolation.h samplen2v.h 
+# jumpword2vec.h
 
-DEPCPP = $(EXSNAPADV)/n2v.cpp $(EXSNAPADV)/word2vec.cpp $(EXSNAPADV)/biasedrandomwalk.cpp $ deeputils.cpp recoveredges.cpp linearinterpolation.cpp samplen2v.cpp jumpword2vec.cpp
+DEPCPP = $(EXSNAPADV)/n2v.cpp $(EXSNAPADV)/word2vec.cpp $(EXSNAPADV)/biasedrandomwalk.cpp $ deeputils.cpp recoveredges.cpp linearinterpolation.cpp samplen2v.cpp 
+# jumpword2vec.cpp
 
 CXXFLAGS += $(CXXOPENMP)
  
