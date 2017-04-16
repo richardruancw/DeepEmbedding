3b61fec49378573b3845a00f01bec9b9457e82c6
Bowen Yao <vinyao1994@gmail.com>
2017-04-16 15:42:11 -0700

    Update test.sh

--- snap/examples/deepembed/test.sh
+++ snap/examples/deepembed/test.sh
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
 ./deepembed -i:graph/facebook_combined.edgelist -o:karate.emb -l:10 -d:24 -p:0.3  -q:0.5 -v -s:100
-=======
-./deepembed -i:graph/facebook_combined.edgelist -o:karate.emb -l:5 -d:24 -p:0.3  -q:0.5 -v -s:10
->>>>>>> origin/master
+
