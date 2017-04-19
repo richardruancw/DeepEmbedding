f83d0c5da163af3048e217396da7f747626e4258
Yue Liu <benliu20131124@gmail.com>
2017-04-19 00:10:45 -0700

    compile tested

---
+++ snap/examples/collapse/test.sh
@@ -0,0 +1,1 @@
+./testgraph -i:graph/facebook_combined.edgelist -o:emb/karate.emb -l:5 -d:24 -p:0.3  -q:0.5 -v
--- snap/examples/deepembed/collapse/test.sh
+++
@@ -1,1 +0,0 @@
-./testgraph -i:graph/facebook_combined.edgelist -o:emb/karate.emb -l:5 -d:24 -p:0.3  -q:0.5 -v
