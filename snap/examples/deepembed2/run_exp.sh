ea385950783eaaffe739582dfa20cdf283925c60
Rciahrdruancw <ruanchuanwei@gmail.com>
2017-04-26 22:25:24 -0700

    fix

--- snap/examples/deepembed2/run_exp.sh
+++ snap/examples/deepembed2/run_exp.sh
@@ -1,5 +1,7 @@
 #!/bin/bash 
 
+DO_PARTITION="DO"
+
 # Learning n2v
 GRAPH_NAME=facebook_combined.edgelist
 RESULT_NAME=test_result
@@ -34,7 +36,10 @@ EMBEDDING_PREFIX="outemb"
 
 TXT=".txt"
 
-python split.py $NUM_PARTITION $GRAPH_FULL_PATH
+if [$DO_PARTITION = "DO"]
+then
+	python split.py $NUM_PARTITION $GRAPH_FULL_PATH
+fi
 
 cd stats
 rm *
@@ -50,6 +55,7 @@ do
         ./deepembed2 -i:graph/$GRAPH_TRAIN -out:graphs_folder -o:./embeddings/$EMBEDDING_TRAIN -stats:./stats/stats.txt -l:$NUM_WALKLEN -d:$NUM_DIM -p:$NUM_P_n2v  -q:$NUM_Q_n2v $CHOICE_VERBOSE -nc:$NUM_COM -ut:$NUM_UPDATE_RATE $CHOICE_DIRECTED $CHOICE_WEIGHTED -ours:0
         python get_time_cpp.py
         python down_stream_eval.py -b:$NUM_BATCH_SIZE -d:$NUM_DIM -r:$i -ours:1
+        echo "The following is original node2vec" 
         python down_stream_eval.py -b:$NUM_BATCH_SIZE -d:$NUM_DIM -r:$i -ours:0
 done
 
