1c59b698b014577b112abbca64bce6b5923af1ba
Yue Liu <benliu20131124@gmail.com>
2017-05-03 00:48:05 -0700

    delete

--- snap/examples/deepembed2/setup_2.sh
+++
@@ -1,4 +0,0 @@
-#after finish installing anaconda
-source ~/.bashrc
-#then restart the terminal, after you come back, run setup_3.sh
-
--- snap/examples/deepembed2/setup_3.sh
+++
@@ -1,8 +0,0 @@
-#get snap-python
-wget http://snap.stanford.edu/snappy/release/snap-3.0.2-3.0-centos6.5-x64-py2.6.tar.gz
-tar zxvf snap-3.0.2-3.0-centos6.5-x64-py2.6.tar.gz
-cd snap-3.0.2-3.0-centos6.5-x64-py2.6
-sudo python setup.py install
-cd ..
-sudo rm snap-3.0.2-3.0-centos6.5-x64-py2.6.tar.gz
-# should be good to go
